68f5e706d70cdfb9812a9a60e7163e1c091cbe40
urpitvrm <himanshuverma9170@gmail.com>
2025-02-24 17:37:48 +0530

    class to className

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,101 +5,100 @@ const Ticket = require("../models/ticktModel");
 
 // register a new user
 exports.registeruser = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-        // res.send(user);
-        sendToken(user, 201, res);
-
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
-
+  console.log(req.body);
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    // res.send(user);
+    sendToken(user, 201, res);
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 };
 
 // login a user
 exports.loginuser = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            res.status(401).send({ success: false, message: "Please enter your email and password" });
-            return;
-        }
-        const user = await User.findOne({ email }).select("+password");
-        if (!user) {
-            res.status(401).send({ success: false, message: "Invalid credentials" });
-            return;
-        }
-        const isPasswordMatch = await user.passwordCompare(password);
-        if (!isPasswordMatch) {
-            res.status(401).send({ success: false, message: "Invalid User" });
-            return;
-        }
-        sendToken(user, 200, res);
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
+  try {
+    const { email, password } = req.body;
+    console.log(req.body);
+    if (!email || !password) {
+      res.status(401).send({
+        success: false,
+        message: "Please enter your email and password",
+      });
+      return;
     }
-
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      res.status(401).send({ success: false, message: "Invalid credentials" });
+      return;
+    }
+    const isPasswordMatch = await user.passwordCompare(password);
+    if (!isPasswordMatch) {
+      res.status(401).send({ success: false, message: "Invalid User" });
+      return;
+    }
+    sendToken(user, 200, res);
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 };
 
 // logout a user
 exports.userlogout = async (req, res, next) => {
-    try {
-        res.cookie("token", null, {
-            expires: new Date(Date.now()),
-            httpOnly: true,
-        });
-        res.status(200).json({
-            success: true,
-            message: "logout successfully",
-        });
-
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
-
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "logout successfully",
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 };
-
 
 //get user details
 exports.getuserdetails = async (req, res, next) => {
-    try {
-        const user = await User.findOne({ _id: req.user.id });
-        res.status(200).json({
-            success: true,
-            user,
-        });
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
+  try {
+    const user = await User.findOne({ _id: req.user.id });
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 };
 
 exports.getuserdashboard = async (req, res, next) => {
-    try {
-        const ticketIds = await Ticket.find({ user: req.params.userId });
+  try {
+    const ticketIds = await Ticket.find({ user: req.params.userId });
 
-        const tickets = await Promise.all(
-            ticketIds.map(async (ticket) => {
-                const movie = await Movie.findOne({ _id: ticket.movie });
-                return {
-                    movieName: movie.name,
-                    ticketId: ticket._id,
-                    seatNumber: ticket.tiketId,
-                    movieTime: movie.timeSlot,
-                    bookingTime: ticket.bookedAt,
-                };
-            })
-        );
+    const tickets = await Promise.all(
+      ticketIds.map(async (ticket) => {
+        const movie = await Movie.findOne({ _id: ticket.movie });
+        return {
+          movieName: movie.name,
+          ticketId: ticket._id,
+          seatNumber: ticket.tiketId,
+          movieTime: movie.timeSlot,
+          bookingTime: ticket.bookedAt,
+        };
+      })
+    );
 
-        res.status(200).json({
-            success: true,
-            tickets,
-        });
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
+    res.status(200).json({
+      success: true,
+      tickets,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: error.message });
+  }
 };
--- frontend/src/pages/auth/SignIn.jsx
+++ frontend/src/pages/auth/SignIn.jsx
@@ -1,66 +1,104 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-    const [email, setemail] = useState();
-    const navigate = useNavigate();
-    const [password, setpassword] = useState();
-    const handlesubmit=async(e)=>{
-        e.preventDefault();
-        if(!email || !password){
-            alert("Please enter your email and password");
-            return;
-        }
-        try {
-            const request = await axios.post(
-              "https://moviezone-sf85.onrender.com/api/v1/login",
-              {
-                email,
-                password,
-              }
-            );
-            localStorage.setItem("user", JSON.stringify(request.data.user))
-            alert("Logged in successfully");
-            navigate("/");
-            window.location.reload();
-        } catch (error) {
-            console.log(error);
-            alert("Invalid credentials. Please try again.");
-        }
+  const [email, setemail] = useState();
+  const navigate = useNavigate();
+  const [password, setpassword] = useState();
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert("Please enter your email and password");
+      return;
     }
-    return (
-        <>
-            <section class="bg-gray-50">
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    
-                    <div class="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-100">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                                🔑 Welcome Back to MovieZone!
-                            </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
-                                <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 " >✉️ Your Email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-400 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="true" value={email} onChange={(e)=>setemail(e.target.value)} />
-                                </div>
-                                <div>
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">🔒 Your Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-400 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true"  value={password} onChange={(e)=>setpassword(e.target.value)}/>
-                                </div>
-                                
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-500" onClick={handlesubmit}>🚀 Log In</button>
-                                <p class="text-sm font-light text-gray-700 dark:text-gray-700">
-                                    create new account? <Link to="/signup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Click here</Link>
-                                </p>
-                            </form>
-                        </div>
-                    </div>
+    try {
+      const request = await axios.post(
+        "https://moviezone-sf85.onrender.com/api/v1/login/",
+        {
+          email,
+          password,
+        }
+      );
+      localStorage.setItem("user", JSON.stringify(request.data.user));
+      alert("Logged in successfully");
+      navigate("/");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+      alert("Invalid credentials. Please try again.");
+    }
+  };
+  return (
+    <>
+      <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-100">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                🔑 Welcome Back to MovieZone!
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    ✉️ Your Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-400 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required="true"
+                    value={email}
+                    onChange={(e) => setemail(e.target.value)}
+                  />
                 </div>
-            </section>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    🔒 Your Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-400 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required="true"
+                    value={password}
+                    onChange={(e) => setpassword(e.target.value)}
+                  />
+                </div>
 
-        </>
-    )
-}
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-500"
+                  onClick={handlesubmit}
+                >
+                  🚀 Log In
+                </button>
+                <p className="text-sm font-light text-gray-700 dark:text-gray-700">
+                  create new account?{" "}
+                  <Link
+                    to="/signup"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Click here
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default SignIn
+export default SignIn;
